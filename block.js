821deba83975b4b95b9474b4543ceea0d1afb1e7
Ralf Anton Beier <ralf_beier@me.com>
2018-05-15 06:43:27 +0200

    Cleaned up warnings from codacy.
    Mainly "Strings must be doublequotes"

--- block.js
+++ block.js
@@ -1,4 +1,4 @@
-const NodeRSA = require('node-rsa');
+const NodeRSA = require("node-rsa");
 
 class Block {
     constructor(timestamp, lastHash, hash, data) {
@@ -17,19 +17,19 @@ class Block {
 
     static genesis() {
         const key = new NodeRSA({b: 512});
-        const text = 'Genesis block!';
-        const encrypted = key.encrypt(text, 'base64');
-        return new this('Genesis time', '-----', 'f1r57-h45h', [text, encrypted]);
+        const text = "Genesis block!";
+        const encrypted = key.encrypt(text, "base64");
+        return new this("Genesis time", "-----", "f1r57-h45h", [text, encrypted]);
     }
 
     static mineBlock(lastBlock, data) {
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = 'todo-hash';
+        const hash = "todo-hash";
         return new this(timestamp, lastHash, hash, data);
 
 
     }
 }
 
-module.exports  = Block
+module.exports  = Block;
